6e9ea1aa38954c95257af7cbd8dfc0697cb18629
Stanley Lewis Jr <stanmanga79@gmail.com>
2019-10-24 19:57:32 -0400

    Week 11 Solutions 1-3

--- 01-Class-Content/11-js-constructors/01-Activities/02-RainingCatsAndDogs-Con/rainingCatsAndDogs-con.js
+++ 01-Class-Content/11-js-constructors/01-Activities/02-RainingCatsAndDogs-Con/rainingCatsAndDogs-con.js
@@ -1,6 +1,6 @@
 // constructor which can be used to create objects with the ".raining", ".noise",
 // and ".makenoise" properties
-function Animal(raining, noise) {
+function Animal1(raining, noise) {
   this.raining = raining;
   this.noise = noise;
   this.makeNoise = function() {
@@ -10,9 +10,33 @@ function Animal(raining, noise) {
   };
 }
 
+class Animal {
+  constructor (raining, noise) {
+    this.raining = raining;
+    this.noise = noise;
+  }
+
+  makeNoise () {
+    if (this.raining) console.log(this.noise);
+    
+  }
+}
+
+class Dog extends Animal {
+  constructor (raining = false, noise = 'Meow!') {
+    super(raining, noise);
+  }
+}
+
+class Cat extends Animal {
+  constructor (raining = true, noise = 'Woof!') {
+    super(raining, noise);
+  }
+}
+
 // sets the variables "dogs" and "cats" to be animal objects and initializes them with raining and noise properties
-var dogs = new Animal(true, "Woof!");
-var cats = new Animal(false, "Meow!");
+var dogs = new Dog(true, "Woof!");
+var cats = new Cat(false, "Meow!");
 
 // calling dogs and cats makeNoise methods
 dogs.makeNoise();
---
+++ 01-Class-Content/11-js-constructors/01-Activities/04-Tamagotchi/digitalPal.js
@@ -0,0 +1,28 @@
+var DigitalPal = function () {
+    this.hungry = false;
+    this.sleepy = false;
+    this.bored = true;
+    this.age = 0;
+
+    this.feed = function () {}
+
+    this.sleep = function () {}
+
+    this.play = function () {}
+}
+
+class DigitalPal {
+    constructor () {
+        this.hungry = false;
+        this.sleepy = false;
+        this.bored = true;
+        this.age = 0;
+    }
+    
+    feed () {}
+
+    sleep () {}
+
+    play () {}
+
+}
