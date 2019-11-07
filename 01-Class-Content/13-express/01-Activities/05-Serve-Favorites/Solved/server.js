636142af2373966afb3a6bb6890b59d73b8865fe
Lori Culberson <bigskybandit@sbcglobal.net>
2019-11-06 21:10:52 -0500

    Update exercise 5

--- 01-Class-Content/13-express/01-Activities/05-Serve-Favorites/Solved/server.js
+++ 01-Class-Content/13-express/01-Activities/05-Serve-Favorites/Solved/server.js
@@ -2,13 +2,11 @@
 var http = require("http");
 var fs = require("fs");
 
-// Set our port to 8080
-var PORT = 8080;
+//TO DO: Set our port to 8080
 
-var server = http.createServer(handleRequest);
+//TO DO: create server
 
 function handleRequest(req, res) {
-
   // Capture the url the request is made to
   var path = req.url;
 
@@ -22,31 +20,14 @@ function handleRequest(req, res) {
       res.end(data);
     });
 
-  case "/movies":
-    return fs.readFile(__dirname + "/movies.html", function(err, data) {
-      if (err) throw err;
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(data);
-    });
+    /*TO DO: 
+      add path for movies.html
+      add path for frameworks.html
+      default to rendering index.html, if none of above cases are hit
+    */
 
-  case "/frameworks":
-    return fs.readFile(__dirname + "/frameworks.html", function(err, data) {
-      if (err) throw err;
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(data);
-    });
-
-    // default to rendering index.html, if none of above cases are hit
-  default:
-    return fs.readFile(__dirname + "/index.html", function(err, data) {
-      if (err) throw err;
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(data);
-    });
   }
 }
 
 // Starts our server.
-server.listen(PORT, function() {
-  console.log("Server is listening on PORT: " + PORT);
-});
+
